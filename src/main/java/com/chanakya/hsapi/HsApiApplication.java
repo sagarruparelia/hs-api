412eecefe6a9ef742a7e5624633d55a80ab95122
Sagar <ruparelia.sagar@gmail.com>
2026-03-04 10:07:10 -0500

    Initial commit

---
+++ src/main/java/com/chanakya/hsapi/HsApiApplication.java
@@ -0,0 +1,13 @@
+package com.chanakya.hsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chanakya/hsapi/HsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanakya.hsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
